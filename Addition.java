d3526a56d854298e16e1056ce8edb490e690734f
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-01-28 03:53:59 +0600

    Java Addition

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String args[]){
+        int a = 10;
+        /*
+         * This is a comment
+         * This is a comment
+         */
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of a and b is: " + sum); //Prints the sum of a and b
+    }
+}
