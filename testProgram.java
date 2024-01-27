b47e0ec73921f0e36d3464b381eb9c2e45380349
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-01-28 04:17:20 +0600

    Greeting for Name

--- testProgram.java
+++ testProgram.java
@@ -1,6 +1,16 @@
-public class testProgram {
+import java.util.Scanner;
+
+public class TestProgram {
     public static void main(String[] args) {
-        System.out.println("Test Run");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your Name: ");
+        // Read the user's name as a String
+        String name = scan.next(); // or scan.nextLine() if the name can have spaces
+
+        System.out.println("Greetings " + name + "!");
+
+        // Close the scanner to avoid resource leakage
+        scan.close();
     }
-    
 }
